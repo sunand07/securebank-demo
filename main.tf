0e41a6de7f612435fc62d6285e920baecb1e5f85
sunand07 <139065922+sunand07@users.noreply.github.com>
2025-10-26 03:02:00 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,23 @@
-provider "aws" {
-    region = "us-east-1"  # Set your desired AWS region
+provider "aws"  {
+  region = "us-east-1"
+}
+resource "aws_instance" "securebank" {
+  ami = var.ami_value
+  instance_type = var.instance_type_value
+  subnet_id = var.subnet_id_value 
 }
 
-resource "aws_instance" "example" {
-    ami           = "ami-0c55b159cbfafe1f0"  # Specify an appropriate AMI ID
-    instance_type = "t2.micro"
+resource "aws_s3_bucket" "s3_bucket_for_demo_demo_securebank" {
+bucket = "s3-bucket-for-demo-demo123"
+}
+
+resource "aws_dynamodb_table" "terraform_lock" {
+name = "terraform_lock"
+billing_mode = "Pay_per_request"
+hash_key = "Lock_ID"
+
+attribute {
+  name = "Lock_ID"
+  type = "S"
+}
 }
