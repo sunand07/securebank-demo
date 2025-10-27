1f3f264cb636599b3fe6fc79089bdf5569c185a2
sunand07 <139065922+sunand07@users.noreply.github.com>
2025-10-28 01:15:03 +0530

    Update main.tf

--- securebank-s3-locking-demo/main.tf
+++ securebank-s3-locking-demo/main.tf
@@ -2,14 +2,14 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "abhishek" {
+resource "aws_instance" "securebank" {
   instance_type = "t2.micro"
   ami = "ami-053b0d53c279acc90" # change this
   subnet_id = "subnet-019ea91ed9b5252e7" # change this
 }
 
 resource "aws_s3_bucket" "s3_bucket" {
-  bucket = "abhishek-s3-demo-xyz" # change this
+  bucket = "securebank-s3-demo-bucket" # change this
 }
 
 resource "aws_dynamodb_table" "terraform_lock" {
