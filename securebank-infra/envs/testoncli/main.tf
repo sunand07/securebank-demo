9114eaa430ad4a4defbb00b60b6c92763c67cbde
sunand07 <139065922+sunand07@users.noreply.github.com>
2025-10-30 00:20:19 +0530

    Create main.tf

---
+++ securebank-infra/envs/testoncli/main.tf
@@ -0,0 +1,22 @@
+module "networking" {
+  source               = "../../modules/networking"
+  cidr_block           = "10.0.0.0/16"
+  vpc_name             = "acme-vpc"
+  public_subnet_count  = 2
+  private_subnet_count = 2
+}
+
+module "security" {
+  source  = "../../modules/security"
+  vpc_id  = module.networking.vpc_id
+  sg_name = "acme-web-sg"
+}
+
+module "compute" {
+  source = "../../modules/compute"
+  ami_id             = var.ami_id
+  instance_type      = var.instance_type
+  subnet_id          = module.networking.public_subnet_ids[0]
+  security_group_ids = [module.security.security_group_id]
+  key_name           = var.key_name
+}
