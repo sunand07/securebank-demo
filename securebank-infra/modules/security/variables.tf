00ec1c5a9f116fb2ddb5ac4ca55be2c261dce980
sunand07 <139065922+sunand07@users.noreply.github.com>
2025-10-30 00:14:00 +0530

    Create variables.tf

---
+++ securebank-infra/modules/security/variables.tf
@@ -0,0 +1,9 @@
+variable "vpc_id" {
+  description = "The VPC ID where the security group will be created"
+  type        = string
+}
+
+variable "sg_name" {
+  description = "The name of the security group"
+  type        = string
+}
